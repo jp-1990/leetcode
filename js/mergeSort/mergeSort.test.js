0130dcdf104501ce6d77c0870f831bf8a72c49fd
jp-1990 <plum_1990@hotmail.co.uk>
2020-12-27 19:22:17 +0000

    update added describe

--- js/mergeSort/mergeSort.test.js
+++ js/mergeSort/mergeSort.test.js
@@ -1,25 +1,27 @@
 const mergeSort = require('./mergeSort');
 
-test('merge sort empty array', () => {
-  expect(mergeSort([])).toStrictEqual([]);
-});
+describe('testing mergeSort', () => {
+  test('merge sort empty array', () => {
+    expect(mergeSort([])).toStrictEqual([]);
+  });
 
-test('merge sort single item array', () => {
-  expect(mergeSort([1])).toStrictEqual([1]);
-});
+  test('merge sort single item array', () => {
+    expect(mergeSort([1])).toStrictEqual([1]);
+  });
 
-test('merge sort even length array', () => {
-  expect(mergeSort([1, 3, 2, 4])).toStrictEqual([1, 2, 3, 4]);
-});
+  test('merge sort even length array', () => {
+    expect(mergeSort([1, 3, 2, 4])).toStrictEqual([1, 2, 3, 4]);
+  });
 
-test('merge sort odd length array', () => {
-  expect(mergeSort([1, 3, 2, 4, 5])).toStrictEqual([1, 2, 3, 4, 5]);
-});
+  test('merge sort odd length array', () => {
+    expect(mergeSort([1, 3, 2, 4, 5])).toStrictEqual([1, 2, 3, 4, 5]);
+  });
 
-test('merge sort already sorted array', () => {
-  expect(mergeSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
-});
+  test('merge sort already sorted array', () => {
+    expect(mergeSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
+  });
 
-test('merge sort reversed array', () => {
-  expect(mergeSort([4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4]);
+  test('merge sort reversed array', () => {
+    expect(mergeSort([4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4]);
+  });
 });
